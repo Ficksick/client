3c68e6301e034695a4c380579dd08e9b3c3469eb
Nolick <mishahoroshun123@gmail.com>
2023-11-23 22:08:00 +0300

    test comit

--- src/main/java/Frames/UsersInformationForAdminFrame.java
+++ src/main/java/Frames/UsersInformationForAdminFrame.java
@@ -133,8 +133,6 @@ public class UsersInformationForAdminFrame extends JFrame {
                 };
                 tableModel.addRow(data);
             }
-
-
         } catch (IOException | ClassNotFoundException ex) {
             ex.printStackTrace();
         }
